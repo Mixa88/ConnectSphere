e9b9d0cbc58e864d2e569617e51568a2cf83b752
Михаил Тихонов <mishkatix88@gmail.com>
2025-08-11 22:44:22 +0300

    Initial Commit

---
+++ ConnectSphere/ConnectSphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectSphereApp.swift
+//  ConnectSphere
+//
+//  Created by Михайло Тихонов on 11.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ConnectSphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
